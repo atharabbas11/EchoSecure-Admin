9baf31feb63970af8c7a4ebfd89a54e78a01505f
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2025-03-02 16:25:04 +0530

    Update adminUserController.js

--- backend/controllers/adminUserController.js
+++ backend/controllers/adminUserController.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import crypto from 'crypto';
 import { sendPasswordSetupEmail } from '../service/otpService.js';
 
-const ECHOSECURE_CLIENT_URL = process.env.ECHOSECURE_CLIENT_URL;
+const CLIENT_URL = process.env.CLIENT_URL;
 
 // Get all users (admin only)
 const getAllUsers = async (req, res) => {
@@ -58,7 +58,7 @@ const createUser = async (req, res) => {
     await newUser.save();
 
     // Send email with password setup link
-    const passwordSetupLink = `${ECHOSECURE_CLIENT_URL}/set-password?token=${passwordSetupToken}`;
+    const passwordSetupLink = `${CLIENT_URL}/set-password?token=${passwordSetupToken}`;
     await sendPasswordSetupEmail(email, passwordSetupLink);
 
     res.status(201).json({
