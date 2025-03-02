efa17cce47a068a9ebe54c1d02fdd0a2dbbe546a
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2025-03-02 16:17:55 +0530

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -32,8 +32,8 @@ export const fetchCsrfToken = async () => {
   try {
     const response = await apiClient.get('/auth/csrf-token', { withCredentials: true });
     const csrfToken = response.data.csrfToken;
-    // apiClient.defaults.headers.common['X-CSRF-Token'] = csrfToken; // Set CSRF token globally
-    // console.log('Fetched CSRF Token:', csrfToken); // Debugging log
+    apiClient.defaults.headers.common['X-CSRF-Token'] = csrfToken; // Set CSRF token globally
+    console.log('Fetched CSRF Token:', csrfToken); // Debugging log
   } catch (error) {
     console.error('Error fetching CSRF token:', error);
   }
