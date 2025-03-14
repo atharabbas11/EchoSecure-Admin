47f3e91a8d0af95ffef0e31a91d6dc3af55fc52c
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2025-03-14 23:12:55 +0530

    Update adminUserRoutes.js

--- backend/routes/adminUserRoutes.js
+++ backend/routes/adminUserRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getAllUsers, createUser, deleteUser } from '../controllers/adminUserController.js'; // Correct import
+import { getAllUsers, createUser, deleteUser, updateUserEmail } from '../controllers/adminUserController.js'; // Correct import
 import { authAdminMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.get("/", authAdminMiddleware, getAllUsers);
 router.post("/", authAdminMiddleware, createUser);
 router.delete("/:id", authAdminMiddleware, deleteUser);
+router.put("/:id/email", authAdminMiddleware, updateUserEmail);
 
 export default router; // Corrected export for ES Modules
