41816d149f3a5f029cfd7190b72f19f0ee248a83
atharabbas11 <71337742+atharabbas11@users.noreply.github.com>
2025-03-18 01:30:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
       <ErrorBoundary>
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+{/*           <Route path="/signup" element={<Signup />} /> */}
           <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path="/users" element={<ProtectedRoute><UsersList /></ProtectedRoute>} />
         </Routes>
